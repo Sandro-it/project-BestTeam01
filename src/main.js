722e23397b3a9c9c438882a83b6a19893265a0c9
Sandro-it <kozachenkooleksandr@gmail.com>
2023-11-07 21:29:26 +0200

    update-main-js-function

--- src/main.js
+++ src/main.js
@@ -28,4 +28,20 @@
   });
 })();
 
+document.getElementById('goTosignup').addEventListener('click', function () {
+  const targetElement = document.getElementById('signup');
+  if (targetElement) {
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+  }
+});
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
